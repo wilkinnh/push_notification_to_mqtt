4042cf45b0081ebef64840879e358a9a01cfee7d
nate <wilkinnh@gmail.com>
2022-03-16 22:52:07 -0400

    add base project with packages for notification listener and mqtt

---
+++ android/app/src/main/kotlin/com/h4wkd/push_notification_to_mqtt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.h4wkd.push_notification_to_mqtt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:android_notification_listener2/android_notification_listener2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Push Notification Listener to MQTT',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  AndroidNotificationListener _notifications;
+  StreamSubscription<NotificationEventV2> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    startListening();
+  }
+
+  void onData(NotificationEventV2 event) {
+    print(event);
+    print('converting package extra to json');
+    var jsonDatax = json.decode(event.packageExtra);
+    print(jsonDatax);
+  }
+
+  void startListening() {
+    _notifications = new AndroidNotificationListener();
+    try {
+      _subscription = _notifications.notificationStream.listen(onData);
+    } on NotificationExceptionV2 catch (exception) {
+      print(exception);
+    }
+  }
+
+  void stopListening() {
+    _subscription.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+      ),
+    );
+  }
+}
