0cc26939ecc50a431d4d49a961c2272dcc92a2b1
nate <wilkinnh@gmail.com>
2022-03-24 23:18:22 -0400

    roughed in mqtt connection and publishing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:notifier_listener/notifier_listener.dart';
+import 'package:push_notification_to_mqtt/model/notification_mqtt.dart';
+import 'package:built_collection/built_collection.dart';
 
 import 'model/notification.dart' as DataModel;
 
@@ -46,6 +52,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   AndroidNotificationListener? _notifications;
   StreamSubscription<NotifierListenerEvent>? _subscription;
+  List<DataModel.ProcessedNotification> _processedNotifications = List.empty(growable: true);
+  List<NotificationMQTT> _notificationParsers = List.empty(growable: true);
+  MqttServerClient? _mqttServerClient;
 
   @override
   void initState() {
@@ -55,21 +64,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    startMQTTServerClient();
     startListening();
   }
 
-  void onData(NotifierListenerEvent event) {
-    print(event);
+  Future<void> startMQTTServerClient() async {
+    final client = MqttServerClient('home.natewilkinson.com', '');
 
-    final notification = DataModel.Notification((b) => b
-      ..packageName = event.packageName
-      ..message = event.packageMessage
-      ..text = event.packageText
-      ..timestamp = event.timeStamp);
+    /// Set the correct MQTT protocol for mosquito
+    client.setProtocolV311();
+    client.logging(on: false);
+//    client.keepAlivePeriod = 20;
+//    client.onDisconnected = onDisconnected;
+//    client.onSubscribed = onSubscribed;
+//    final connMess = MqttConnectMessage()
+//        .withClientIdentifier('Mqtt_MyClientUniqueIdQ2')
+//        .withWillTopic('willtopic') // If you set this you must set a will message
+//        .withWillMessage('My Will message')
+//        .startClean() // Non persistent session for testing
+//        .withWillQos(MqttQos.atLeastOnce);
+//    print('EXAMPLE::Mosquitto client connecting....');
+//    client.connectionMessage = connMess;
+
+    try {
+      await client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+
+    /// Check we are connected
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+      client.disconnect();
+      return;
+    }
+
+    _mqttServerClient = client;
   }
 
   void startListening() {
-    var listener = new AndroidNotificationListener();
+    var listener = AndroidNotificationListener();
     _notifications = listener;
     try {
       _subscription = listener.notificationStream?.listen(onData);
@@ -82,12 +120,84 @@ class _MyHomePageState extends State<MyHomePage> {
     _subscription?.cancel();
   }
 
+  void onData(NotifierListenerEvent event) {
+    print(event);
+
+    final notification = DataModel.Notification((b) => b
+      ..packageName = event.packageName
+      ..message = event.packageMessage
+      ..text = event.packageText
+      ..timestamp = event.timeStamp);
+
+    processNotification(notification);
+  }
+
+  void processNotification(DataModel.Notification notification) {
+    var matches = List<NotificationMQTT>.empty(growable: true);
+
+    _notificationParsers.forEach((parser) {
+      if (parser.packageName != notification.packageName) {
+        // only match if packageNames match
+        return;
+      }
+      final regex = RegExp(parser.regex);
+      final match = regex.firstMatch(notification.text);
+      // check if first match equals the regexMatch
+      if (match?.group(0) == parser.regexMatch) {
+        matches.add(parser);
+
+        // publish MQTT message
+        publishMQTTMessage(notification, parser);
+      }
+    });
+
+    final processedNotification = DataModel.ProcessedNotification((b) => b
+      ..notification = notification.toBuilder()
+      ..regexMatches = BuiltList<NotificationMQTT>(matches).toBuilder());
+
+    _processedNotifications.insert(0, processedNotification);
+  }
+
+  Future<void> publishMQTTMessage(DataModel.Notification notification, NotificationMQTT parser) async {
+    final builder1 = MqttClientPayloadBuilder();
+    builder1.addString('Hello from mqtt_client topic 1');
+    print('EXAMPLE:: <<<< PUBLISH 1 >>>>');
+    _mqttServerClient?.publishMessage(parser.publishTopic, MqttQos.exactlyOnce, builder1.payload!);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
+        ),
+        body: Table(
+          children: List<TableRow>.generate(_processedNotifications.length, (index) {
+            final processedNotification = _processedNotifications[index];
+            return TableRow(children: [
+              ListView(
+                children: [
+                  Text('PackageName: ' + processedNotification.notification.packageName),
+                  Text('Text: ' + processedNotification.notification.text),
+                  Text('Message: ' + processedNotification.notification.message),
+                  Text('Timestamp: ' + processedNotification.notification.timestamp.toString()),
+                ],
+              )
+            ]);
+          }),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.add),
+          onPressed: () {
+            final event = NotifierListenerEvent(
+                packageName: 'com.h4wkd.test',
+                packageText: 'This is my text',
+                packageMessage: 'This is my message',
+                timeStamp: DateTime.now());
+            onData(event);
+          },
         ),
       ),
     );
